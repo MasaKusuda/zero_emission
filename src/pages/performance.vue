a6a7e9f1428cd6468d1b9b4805974367e5f3848f
MasaKusu <masa.kusuda.1999@gmail.com>
2022-05-20 16:55:39 +0900

    add inprogress page

--- src/pages/performance.vue
+++ src/pages/performance.vue
@@ -1,10 +1,12 @@
-<template><div></div></template>
+<template>
+  <v-app>
+    <p class="text-h2" style="text-align: center">このページは工事中です</p>
+    <v-btn x-large link to="/">HOME PAGE</v-btn>
+  </v-app>
+</template>
 
 <script>
 export default {
-  name: 'PerformancePage',
-  layout: 'article',
+  name: 'InprogressPage',
 }
 </script>
-
-<style scoped></style>
