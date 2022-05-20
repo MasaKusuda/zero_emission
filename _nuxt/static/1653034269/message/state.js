bbb64dc4169eed91895f31536b907b4c87bdca02
MasaKusu <masa.kusuda.1999@gmail.com>
2022-05-20 17:14:11 +0900

    Updates

--- _nuxt/static/1653034269/message/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fzero_emission\u002F_nuxt\u002Fstatic\u002F1653034269",layout:"default",error:a,serverRendered:true,routePath:"\u002Fmessage",config:{_app:{basePath:"\u002Fzero_emission\u002F",assetsPath:"\u002Fzero_emission\u002F_nuxt\u002F",cdnURL:a}}}}(null));
---
+++ _nuxt/static/1653034446/manifest.js
@@ -0,0 +1,1 @@
+__NUXT_JSONP__("manifest.js", {routes:["\u002F","\u002Fperformance","\u002Fmessage","\u002Ftest\u002Farticle","\u002Fconcept","\u002Ftest\u002Findex_copy","\u002Ftest","\u002Fcontact","\u002Fmember","\u002Ftest\u002Fdcopy","\u002Ftest\u002Finspire"]})
