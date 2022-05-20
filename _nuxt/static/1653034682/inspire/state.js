075a162bd7a0b919ff54c711ef7e85a7da809221
MasaKusu <masa.kusuda.1999@gmail.com>
2022-05-20 17:25:16 +0900

    Updates

--- _nuxt/static/1653034682/inspire/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fzero_emission\u002F_nuxt\u002Fstatic\u002F1653034682",layout:"empty",error:{statusCode:404,path:a,message:"This page could not be found"},serverRendered:false,routePath:a,config:{_app:{basePath:"\u002Fzero_emission\u002F",assetsPath:"\u002Fzero_emission\u002F_nuxt\u002F",cdnURL:null}}}}("\u002Finspire"));
